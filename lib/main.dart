0616a040390583ab707cc71d51a462447136d912
Eduardo G. S. Pereira <edu_vcd@hotmail.com>
2026-02-09 12:06:00 +0000

    chore: copy pasta from gpt

--- lib/main.dart
+++ lib/main.dart
@@ -1,121 +1,107 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
+import 'dart:async';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-void main() {
-  runApp(const MyApp());
+final hrService = Uuid.parse('b1999fa6-7006-4bbd-94bd-390d103a13e6');
+final hrChar = Uuid.parse('cb616e20-9ecc-490f-92ad-e08992dd688a');
+
+void main() => runApp(const _App());
+
+class _App extends StatefulWidget {
+  const _App();
+
+  @override
+  State<_App> createState() => _S();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class _S extends State<_App> {
+  final ble = FlutterReactiveBle();
+  StreamSubscription<DiscoveredDevice>? scanSub;
+  StreamSubscription<ConnectionStateUpdate>? connSub;
+  StreamSubscription<List<int>>? hrSub;
 
-  // This widget is the root of your application.
+  String? id;
+  int bpm = 0;
+  String status = 'tap';
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  void dispose() {
+    hrSub?.cancel();
+    connSub?.cancel();
+    scanSub?.cancel();
+    super.dispose();
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  Future<void> go() async {
+    await Permission.bluetoothScan.request();
+    await Permission.bluetoothConnect.request();
+    await Permission.locationWhenInUse.request();
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+    bpm = 0;
+    id = null;
+    status = 'scan';
+    setState(() {});
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+    scanSub?.cancel();
+    scanSub = ble.scanForDevices(withServices: [hrService]).listen((d) {
+      if (id != null) return;
+      id = d.id;
+      scanSub?.cancel();
+      status = 'conn';
+      setState(() {});
 
-  final String title;
+      connSub?.cancel();
+      connSub = ble.connectToDevice(id: d.id).listen((u) {
+        if (u.connectionState == DeviceConnectionState.connected) {
+          status = 'ok';
+          setState(() {});
+          final q = QualifiedCharacteristic(
+            characteristicId: hrChar,
+            serviceId: hrService,
+            deviceId: d.id,
+          );
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+          hrSub?.cancel();
+          hrSub = ble.subscribeToCharacteristic(q).listen((data) {
+            final f = data.isNotEmpty ? data[0] : 0;
+            final is16 = (f & 0x01) == 1;
+            if (!is16 && data.length >= 2) bpm = data[1];
+            if (is16 && data.length >= 3) bpm = data[1] | (data[2] << 8);
+            setState(() {});
+          });
+        }
+        if (u.connectionState == DeviceConnectionState.disconnected) {
+          status = 'disc';
+          id = null;
+          setState(() {});
+        }
+      });
+    });
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+    Future.delayed(const Duration(seconds: 8), () {
+      if (id == null) {
+        scanSub?.cancel();
+        status = 'none';
+        setState(() {});
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return GestureDetector(
+      onTap: go,
+      child: ColoredBox(
+        color: const Color(0xFF000000),
+        child: Center(
+          child: Text(
+            bpm == 0 ? '--' : '$bpm',
+            textDirection: TextDirection.ltr,
+            style: const TextStyle(fontSize: 120, color: Color(0xFFFFFFFF)),
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
